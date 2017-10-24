2d8bdb95011f58698c3a83ebde19268e5f246704
tanchengwei <804589369@qq.com>
2017-10-24 17:33:17 +0800

    files via update

--- public/script/common.js
+++ public/script/common.js
@@ -7,4 +7,28 @@
         $('#mask').addClass('not-display')
         $('#content').css('max-height', 'none')
     })
+
+    function closure() {
+        var fixed = false
+        return function (event) {
+            if ($(window).scrollTop() > 61) {
+                if (!fixed) {
+                    fixed = true
+                    $('#classification').css('top', '15px')
+                    $('#classification').css('left', $('#classification').offset().left + 'px')
+                    $('#classification').addClass('fixed')
+                }
+            } else {
+                if (fixed) {
+                    fixed = false
+                    $('#classification').css('top', '')
+                    $('#classification').css('left', '')
+                    $('#classification').removeClass('fixed')
+                }
+            }
+        }
+    }
+
+    window.addEventListener('scroll', closure())
+
 })()
